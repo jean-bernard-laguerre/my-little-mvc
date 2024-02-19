1018367abe78b55d3a25fa179f44ac1ad6aa0b55
Gibet13 <48248176+Gibet13@users.noreply.github.com>
2024-02-19 11:11:58 +0100

    Commentaires et typage

--- job-06/src/Model/Interface/EntityInterface.php
+++
@@ -1,5 +0,0 @@
-<?php namespace App;
-    interface EntityInterface {
-        public function getId(): int;
-        public function setId(int $id): void;
-    }
--- job-07/src/Model/Interface/StockableInterface.php
+++
@@ -1,5 +0,0 @@
-<?php namespace App;
-    interface StockableInterface {
-        public function addStock(int $quantity): void;
-        public function removeStock(int $quantity): void;
-    }
